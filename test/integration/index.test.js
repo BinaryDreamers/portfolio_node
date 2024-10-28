d0e341cb4c00ebd134d2b277c1b0a5a7709a9c53
Eyosiyas <eyosiyashabtemariam@gmail.com>
2024-10-28 09:15:00 +0300

    Added config

---
+++ test/integration/index.test.js
@@ -0,0 +1,10 @@
+const request = require("supertest");
+let server;
+
+describe("Check all endpoints", () => {
+  // beforeEach(() => (server = require("../index")));
+  // afterEach(() => {
+  //   server.close();
+  // });
+  it("", () => {});
+});
